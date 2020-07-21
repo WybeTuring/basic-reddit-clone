0e79d2d5694914963d8e6e7f57fca7ba943de46b
Karl Lemfon <karllemfon@gmail.com>
2020-07-21 03:37:58 +0000

    Finished the layout of the home page, and went on to build the form control of the search bar. The important thing to note here is that instead of using the FormControl and observables, it is possible to have instead used the ngModel and bound the input in the form. However, the advantage with using the observable is that it gives us the ability to launch action by listening to an observable, which  is more sleek.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,69 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from "@ionic/angular";
+import { DataService } from "../services/data.service";
+import { RedditService } from "../services/reddit.service";
+import { FormControl, FormGroup } from "@angular/forms";
+import { Plugins } from "@capacitor/core";
+
+import { SettingsPage } from "../settings/settings.page";
+
+import { debounceTime } from "rxjs/operators";
+import { distinctUntilChanged } from "rxjs/operators";
+
+const { Browser, Keyboard } = Plugins;
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  public subredditForm: FormGroup;
 
+  constructor(
+    private dataServie: DataService,
+    public redditService: RedditService,
+    private modalCtrl: ModalController
+  ) {
+    this.subredditForm = new FormGroup({
+      subredditControl: new FormControl("")
+    });
+  }
+
+  ngOnInit(){
+
+    this.redditService.load();
+
+    this.subredditForm
+    .get("subredditControl")
+    .valueChanges.pipe(
+      debounceTime(1500),
+      distinctUntilChanged()
+    )
+    .subscribe((subreddit: any) => {
+      if(subreddit.lenth > 0){
+        this.redditService.changeSubreddit(subreddit);
+        Keyboard.hide().catch(err => {
+          console.warn(err);
+        });
+      }
+    });
+  }
+
+  showComments(post): void{
+
+  }
+
+  openSettings(): void {
+
+  }
+
+  playVideo(e:any, post:any): void{
+
+  }
+
+  loadMore(): void{
+
+  }
 }
